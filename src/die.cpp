ede3abc85a054d26d880b4f156b2cb929ff31efd
esleiderZ <esleider.duboy@g.austincc.edu>
2025-12-09 09:22:30 +0000

    Die.H

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,14 @@
-//
+#include <cstdlib>
+#include <ctime>
+#include "die.h"
+
+int Die::roll()
+{
+    static bool seeded = false;
+    if (!seeded)
+    {
+        std::srand(std::time(nullptr));
+        seeded = true;
+    }
+    return std::rand() % sides + 1;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,16 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "die.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+    REQUIRE(true == true);
+}
+
+TEST_CASE("Die rolls return values 1–6") {
+    Die d;
+    for(int i = 0; i < 10; i++) {
+        int value = d.roll();
+        REQUIRE(value >= 1);
+        REQUIRE(value <= 6);
+    }
 }
